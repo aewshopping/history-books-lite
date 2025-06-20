a30f7eb8a6b9834231e4b0191802e62acbb79030
aewshopping <aewshopping@gmail.com>
2025-06-20 11:51:37 +0100

    Update build-db.sh

--- build-db.sh
+++ build-db.sh
@@ -1,5 +1,5 @@
-sqlite-utils insert data.db books ./data/books.tsv --tsv
+sqlite-utils insert data.db books ./data/popular-history-books.tsv --tsv --pk isbn_10
 sqlite-utils insert data.db books_tags ./data/books-tags.csv --csv
 sqlite-utils insert data.db quotes ./data/quotes.tsv --tsv
-sqlite-utils insert data.db tags ./data/tags.csv --csv
-sqlite-utils insert data.db cats ./data/cats.csv --csv
+sqlite-utils insert data.db tags ./data/tags.csv --csv --pk pk_tag_id
+sqlite-utils insert data.db cats ./data/cats.csv --csv --pk pk_cat_id
